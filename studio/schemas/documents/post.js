7e76d861b72da3fec662faabb727a8b54771e15f
hnwong28 <hnwong28@gmail.com>
2021-07-02 19:22:43 +0800

    Update post.js

--- studio/schemas/documents/post.js
+++ studio/schemas/documents/post.js
@@ -45,6 +45,11 @@ export default {
       title: 'Main image'
     },
     {
+      name: 'body',
+      type: 'bodyPortableText',
+      title: 'Body'
+    },
+    {
       name: 'excerpt',
       type: 'excerptPortableText',
       title: 'Excerpt',
@@ -73,11 +78,6 @@ export default {
           }
         }
       ]
-    },
-    {
-      name: 'body',
-      type: 'bodyPortableText',
-      title: 'Body'
     }
   ],
   orderings: [
